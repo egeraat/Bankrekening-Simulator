0e19ed8a64c21e1e9590e4dfd9b091270578f9da
egeraat <d281067@outlook.com>
2024-09-12 12:49:15 +0200

    BankAccount.cs aangemaakt zodat het overzichtelijker is

---
+++ Bankrekening Simulator/BankAccount.cs
@@ -0,0 +1,47 @@
+﻿namespace BankRekeningSimulator
+{
+    class BankAccount
+    {
+
+
+        private decimal balance; //private variabel zodat de gegevens beschermd worden tegen ongewenste wijzingen buitenaf
+
+
+        public BankAccount(decimal startsaldo)
+        {
+            balance = startsaldo;
+        }
+
+        public void Deposit(decimal hoeveelheid) //methode Deposit
+        {
+            if (hoeveelheid > 0)
+            {
+                balance += hoeveelheid; //voegt het bedrag toe aan de hoeveelheid geld we hebben 
+                Console.WriteLine($"{hoeveelheid} is gestort op je rekening. Je hebt nu in totaal {balance}");
+            }
+            else
+            {
+                Console.WriteLine("Het moet een getal zijn en het getal moet positief zijn");
+            }
+        }
+
+        public void Withdraw(decimal hoeveelheid)
+        {
+            if (hoeveelheid > 0)
+            {
+                balance -= hoeveelheid; //hetzelfde als net alleen nu withdraw dus veranderen we + in -
+                Console.WriteLine($"{hoeveelheid} is opgenomen van je rekening. Je hebt nu in totaal {balance}");
+            }
+            else
+            {
+                Console.WriteLine("Het moet een getal zijn en het getal moet positief zijn");
+            }
+        }
+
+        // Methode ShowBalance aangepast zodat deze geen parameter vereist
+        public void ShowBalance()
+        {
+            Console.WriteLine($"Je huidige saldo is: {balance}");
+        }
+    }
+}
--- Bankrekening Simulator/Program.cs
+++ Bankrekening Simulator/Program.cs
@@ -1,50 +1,14 @@
 ﻿using System;
-using System.Security.Cryptography.X509Certificates;
 
 namespace BankRekeningSimulator
 {
-    class BankAccount
+    class Program
     {
-        private decimal balance; //private variabel zodat de gegevens beschermd worden tegen ongewenste wijzingen buitenaf
-
-        public void Deposit(decimal hoeveelheid) //methode Deposit
+        static void Main(string[] args) // static = direct aan de class gekoppeld, niet aan een object van die class
         {
-            if (hoeveelheid > 0)
-            {
-                balance += hoeveelheid; //voegt het bedrag toe aan de hoeveelheid geld we hebben 
-                Console.WriteLine($"{hoeveelheid} is gestort op je rekening. Je hebt nu in totaal {balance}");
-            }
-            else
-            {
-                Console.WriteLine("Het moet een getal zijn en het getal moet positief zijn");
-            }
-        }
+            BankAccount mijnRekening = new BankAccount(100); // Creëert een nieuw BankAccount object met een start saldo van 100
 
-        public void Withdraw(decimal hoeveelheid)
-        {
-            if (hoeveelheid > 0)
-            {
-                balance -= hoeveelheid; //hetzelfde als net alleen nu withdraw dus veranderen we + in -
-                Console.WriteLine($"{hoeveelheid} is opgenomen van je rekening. Je hebt nu in totaal {balance}");
-            }
-            else
-            {
-                Console.WriteLine("Het moet een getal zijn en het getal moet positief zijn");
-            }
-        }
-
-        // Methode ShowBalance aangepast zodat deze geen parameter vereist
-        public void ShowBalance()
-        {
-            Console.WriteLine($"Je huidige saldo is: {balance}");
-        }
-
-        static void Main(string[] args) //static = direct aan de class gekoppeld niet aan een object van die class
-        {
-            BankAccount mijnRekening = new BankAccount(); //Creëert een nieuw BankAccount object
-            mijnRekening.Deposit(100); //stort 100 euro
-
-            while (true)
+            while (true) // Oneindige loop om de gebruiker steeds keuzes te laten maken
             {
                 Console.WriteLine("Maak een keuze hieronder");
                 Console.WriteLine("(1) Saldo bekijken - Show Balance ");
@@ -52,44 +16,44 @@ namespace BankRekeningSimulator
                 Console.WriteLine("(3) Opnemen - withdraw");
                 Console.WriteLine("(4) Exit");
 
-                string keuzemenu = Console.ReadLine(); //Leest de keuze van de gebruiker
+                string keuzemenu = Console.ReadLine(); // Leest de keuze van de gebruiker
 
-                if (keuzemenu == "1") //(1)
+                if (keuzemenu == "1") // (1) Toon het saldo
                 {
-                    mijnRekening.ShowBalance(); // Aangepaste aanroep zonder argument
+                    mijnRekening.ShowBalance(); // Roept de ShowBalance-methode van BankAccount aan
                 }
-                else if (keuzemenu == "2") //(2)
+                else if (keuzemenu == "2") // (2) Geld storten
                 {
-                    Console.WriteLine("Hoeveel wil je storten");
-                    if (decimal.TryParse(Console.ReadLine(), out decimal bedrag))
+                    Console.WriteLine("Hoeveel wil je storten?");
+                    if (decimal.TryParse(Console.ReadLine(), out decimal bedrag)) // Controleert of de input een geldig decimaal getal is
                     {
-                        mijnRekening.Deposit(bedrag);
+                        mijnRekening.Deposit(bedrag); // Stort het bedrag op de rekening
                     }
                     else
                     {
-                        Console.WriteLine("Voer een geldig bedrag in");
+                        Console.WriteLine("Voer een geldig bedrag in.");
                     }
                 }
-                else if (keuzemenu == "3")
+                else if (keuzemenu == "3") // (3) Geld opnemen
                 {
                     Console.WriteLine("Hoeveel wil je opnemen?");
-                    if (decimal.TryParse(Console.ReadLine(), out decimal bedrag))
+                    if (decimal.TryParse(Console.ReadLine(), out decimal bedrag)) // Controleert of de input een geldig decimaal getal is
                     {
-                        mijnRekening.Withdraw(bedrag);
+                        mijnRekening.Withdraw(bedrag); // Neemt het bedrag op van de rekening
                     }
                     else
                     {
-                        Console.WriteLine("Voer een geldig getal in");
+                        Console.WriteLine("Voer een geldig getal in.");
                     }
                 }
-                else if (keuzemenu == "4")
+                else if (keuzemenu == "4") // (4) Stop het programma
                 {
-                    Console.WriteLine("Programma wordt afgesloten");
-                    break; //verlaat loop en stopt het programma
+                    Console.WriteLine("Programma wordt afgesloten.");
+                    break; // Verlaat de loop en stopt het programma
                 }
                 else
                 {
-                    Console.WriteLine("Ongeldige keuze");
+                    Console.WriteLine("Ongeldige keuze."); // In geval van een verkeerde invoer
                 }
             }
         }
